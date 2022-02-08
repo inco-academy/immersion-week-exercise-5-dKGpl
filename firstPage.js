a1fd749247aff4c0799e1b7ad5d3e823c3791baf
darekgrabowski <dariuszgrabowski@mixbudkg.pl>
2022-02-09 00:48:19 +0100

    Forgotten let

--- firstPage.js
+++ firstPage.js
@@ -2,7 +2,7 @@ document.head.title = "My first (dynamic) webpage";
 const allH2 = document.querySelectorAll("h2");
 let buttonThen = "",
     buttonNow = "";
-for (h2 of allH2) {
+for ( const h2 of allH2) {
   if (h2.innerText === "Then") {
     buttonThen = h2.addEventListener("click", () =>
       alert("The Beatles (January 13, 1969)")
